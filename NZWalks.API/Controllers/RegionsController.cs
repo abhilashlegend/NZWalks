7f54ea2c0bda3565302cb09ea06c355c7a93192a
abhilashlegend <abhilash456a@live.com>
2023-09-13 18:35:55 +0530

    Added regions controller

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageUrl = ""
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington",
+                    Code = "WLG",
+                    RegionImageUrl = ""
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
