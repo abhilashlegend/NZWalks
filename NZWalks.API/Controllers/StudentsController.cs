aa867e8bb61aa8987c71e6663a69284f99510195
abhilashlegend <abhilash456a@live.com>
2023-09-13 15:12:39 +0530

    Added models

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult getStudents()
+        {
+            string[] students = new string[] { "Abhilash", "Abhishek", "Anup" };
+            return Ok(students);
+        }
+    }
+}
